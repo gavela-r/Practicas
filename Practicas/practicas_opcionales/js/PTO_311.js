acca7c7c5243cc8125d69818e515be5087eb8a69
Adrian Gavela Rodrigez <adriangavela09@gamil.com>
2024-03-21 08:58:07 +0100

    commit

--- Practicas/practicas_opcionales/js/PTO_311.js
+++ Practicas/practicas_opcionales/js/PTO_311.js
@@ -1,11 +1,17 @@
 function maxSequence(arr){
     let suma = 0;
+    let sumaMax = 0;
     for(let k = 0; k < arr.length; k++){
         suma += arr[k];
+        if(suma < 0){
+            suma = 0;
+        }else if(suma > sumaMax){
+            sumaMax = suma;
+        }
+
         
-        console.log(suma);
     }
-    return suma;
+    return sumaMax;
 }
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
